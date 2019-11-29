4ebaf711e5814fb1e465980fed5e34550b695095
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-11-29 12:44:55 +0100

    populated the module

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # js-framework-benchmark-utils
 An utility to centralize and rule common js-framework-benchmark test cases.
+
+```js
+import Scope from 'js-framework-benchmark-utils';
+// or const Scope = require('js-framework-benchmark-utils');
+
+const scope = Scope(function update({
+
+  // database
+  data,     // the Array containing all items
+  selected, // the currently selected item.id or -1
+
+  // methods (self bound)
+  add,      // add 1000 rows/items to the data
+  clear,    // remove all rows/items from the data
+  remove,   // remove(item.id) to remove only a specific row/item
+  run,      // create 1000 rows/items
+  runLots,  // create 10000 rows/items
+  select,   // select(item.id) to select a specific row/item
+  swapRows, // to swap the second row/item with the one before the last one
+  update    // to update every 10th row/item label
+}) {
+  // this function will be invoked whenever an action/update happens
+  // use the body of this function to render the benchmark table
+});
+```
+
+The `scope` reference can be used to add actions within the `update` function, or outside it, keeping the update related to rows/items changes only.
---
+++ cjs/index.js
@@ -0,0 +1,66 @@
+'use strict';
+let did = 1;
+const buildData = (count) => {
+    const adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
+    const colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
+    const nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
+    const data = [];
+    for (let i = 0; i < count; i++) {
+        data.push({
+            id: did++,
+            label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)]
+        });
+    }
+    return data;
+};
+
+const _random = max => Math.round(Math.random() * 1000) % max;
+
+module.exports = update => {
+  const scope = {
+    add() {
+      scope.data.push(...buildData(1000));
+      update(scope);
+    },
+    clear() {
+      scope.data = [];
+      update(scope);
+    },
+    remove(id) {
+      const {data} = scope;
+      const idx = data.findIndex(d => d.id === id);
+      data.splice(idx, 1);
+      update(scope);
+    },
+    run() {
+      scope.data = buildData(1000);
+      update(scope);
+    },
+    runLots() {
+      scope.data = buildData(10000);
+      update(scope);
+    },
+    select(id) {
+      scope.selected = id;
+      update(scope);
+    },
+    swapRows() {
+      const {data} = scope;
+      if (data.length > 998) {
+        const tmp = data[1];
+        data[1] = data[998];
+        data[998] = tmp;
+      }
+      update(scope);
+    },
+    update() {
+      const {data} = scope;
+      for (let i = 0, {length} = data; i < length; i += 10)
+        data[i].label += ' !!!';
+      update(scope);
+    },
+    selected: -1,
+    data: [],
+  };
+  return scope;
+};
